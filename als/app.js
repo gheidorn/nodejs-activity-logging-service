4e0e06cdc4f65e5396d559e5db47b90682807c2e
Greg Heidorn <greg.heidorn@gmail.com>
2013-04-07 01:19:35 -0500

    replaced tabs with spaces

--- als/app.js
+++ als/app.js
@@ -3,64 +3,64 @@ var express = require('express'), fs = require('fs');
 var app = require('express').createServer();
 
 app.configure(function() {
-	app.use(express.logger('dev'));
-	app.use(express.bodyParser());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
 });
 
 app.use(function(err, req, res, next) {
-	//console.log('bad post body =>');
-	//console.log(req);
-	//console.error(err.stack);
-	res.send(500, 'Invalid JSON submitted.');
+    //console.log('bad post body =>');
+    //console.log(req);
+    //console.error(err.stack);
+    res.send(500, 'Invalid JSON submitted.');
 });
 
 app.get('/', function(req, res) {
-	//console.log('GET /');
+    //console.log('GET /');
     res.send(200);
 });
 
 app.get('/favicon.ico', function(req, res) {
-	// route favicon requests into nothingness
-	//console.log('GET /favicon');
+    // route favicon requests into nothingness
+    //console.log('GET /favicon');
     //res.send(302);
 });
 
 app.post('/als/activity', function(req, res) {
-	//console.log('POST /als/activity');
+    //console.log('POST /als/activity');
 
-	if(req.body.activity.type === 'search') {
-		//console.log('log search activity');
-		// write search activity to mongodb
+    if(req.body.activity.type === 'search') {
+        //console.log('log search activity');
+        // write search activity to mongodb
 
-		var activity = JSON.stringify(req.body.activity) + "\n";
-		var date = new Date();
-		fs.appendFile('search.activity.json', "[" + date +  "] " + activity, function(err){
-			if(err) {
-				console.log('error attempting to write to search.activity.json');
-				console.log(err);
-			} else {
-				//console.log('wrote to log');
-			}
-		});
-	} else if (req.body.activity.type === 'impression') {
-		// write impression activity to file
-		var activity = JSON.stringify(req.body.activity) + "\n";
-		var date = new Date();
-		fs.appendFile('impression.activity.json', "[" + date +  "] " + activity, function(err){
-			if(err) {
-				console.log('error attempting to write to impression.activity.json');
-				console.log(err);
-			} else {
-				//console.log('wrote to log');
-			}
-		});
-	} else {
-		console.log('type not valid or undefined');
-	}
+        var activity = JSON.stringify(req.body.activity) + "\n";
+        var date = new Date();
+        fs.appendFile('search.activity.json', "[" + date +  "] " + activity, function(err){
+            if(err) {
+                console.log('error attempting to write to search.activity.json');
+                console.log(err);
+            } else {
+                //console.log('wrote to log');
+            }
+        });
+    } else if (req.body.activity.type === 'impression') {
+        // write impression activity to file
+        var activity = JSON.stringify(req.body.activity) + "\n";
+        var date = new Date();
+        fs.appendFile('impression.activity.json', "[" + date +  "] " + activity, function(err){
+            if(err) {
+                console.log('error attempting to write to impression.activity.json');
+                console.log(err);
+            } else {
+                //console.log('wrote to log');
+            }
+        });
+    } else {
+        console.log('type not valid or undefined');
+    }
 
 
 
-	res.send(200);
+    res.send(200);
 });
 
 
